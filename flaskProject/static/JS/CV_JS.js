46a4bbe9297d8a280db02aa30eb808ecb49cbde6
rotemyona <rotemyon@post.bgu.ac.il>
2020-12-30 19:16:41 +0200

    update

--- flaskProject/static/JS/CV_JS.js
+++ flaskProject/static/JS/CV_JS.js
@@ -1,9 +1,9 @@
 function changepic1(x) {
-    x.src ='mefunny1.jpg';
+    x.src ="{{ url_for('static', filename='JS/mefunny1.jpg') }}";
 }
 
 function changpic2(x) {
-    x.src ='meforcactus.jpg';
+    x.src ="{{ url_for('static', filename='JS/meforcactus.jpg') }}";
 }
 $(".btn").click(function(){
     $(".input, .area").val("");
